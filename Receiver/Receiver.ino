42ad4468c9763c392641b7419edeab1ea92a0b02
dominic assia <dominicassia@gmail.com>
2021-02-11 11:41:41 -0500

    Workin on communication and pins

--- Receiver/Receiver.ino
+++ Receiver/Receiver.ino
@@ -1,9 +1,40 @@
-void setup() {
-  // put your setup code here, to run once:
+// The goal of this file is to receive data from an Arduino Uno
+//    Receiver    : Arduino Nano
+//    Transmitter : Arduino Uno 
+// ** This file is meant to be uploaded to the Arduino Nano (Receiver)
 
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+#define cePin   7   // D7
+#define csnPin  8   // D8
+#define sckPin  13  // D13
+#define mosiPin 11  // D11
+#define misoPin 12  // D12
+// irq not connected
+
+RF24 radio(cePin, csnPin);
+
+const byte address[6] = "00001";
+
+void setup() {
+  Serial.begin(9600);
+  radio.begin();
+  radio.openReadingPipe(0, address);
+  radio.setPALevel(RF24_PA_MIN);
+  radio.startListening();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  if (radio.available()) {
+    char text[32] = "";
+    radio.read(&text, sizeof(text));
+    Serial.print("Received: ");
+    Serial.println(text);
+    delay(1000);
+  }else{
+    Serial.println("Not Available");
+    delay(1000);
+  }
 }
--- Transmitter/Transmitter.ino
+++ Transmitter/Transmitter.ino
@@ -1,9 +1,33 @@
-void setup() {
-  // put your setup code here, to run once:
+// The goal of this file is to send data from an Arduino Uno
+//    Receiver    : Arduino Nano
+//    Transmitter : Arduino Uno 
+// ** This file is meant to be uploaded to the Arduino Uno (Transmitter)
 
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+#define cePin   8   // D8
+#define csnPin  10  // D10
+#define sckPin  13  // D13
+#define mosiPin 11  // D11
+#define misoPin 12  // D12
+// irq not connected
+
+RF24 radio(cePin, csnPin);
+
+const byte address[6] = "00001";
+
+void setup() {
+  pinMode(csnPin, OUTPUT);
+  radio.begin();
+  radio.openWritingPipe(address);
+  radio.setPALevel(RF24_PA_MIN);
+  radio.stopListening();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  const char text[] = "Hello World";
+  radio.write(&text, sizeof(text));
+  delay(1000);
 }
